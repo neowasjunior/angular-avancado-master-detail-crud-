a1ebc2fb7a91526736bcbeac838bc3e2febab764
WASHINGTON LUIZ SANTOS JUNIOR <wsantos@ad.trt13.jus.br>
2025-02-19 09:53:19 -0300

    adding categories model

---
+++ src/app/pages/categories/shared/category.model.ts
@@ -0,0 +1,9 @@
+import { SplitInterpolation } from "@angular/compiler";
+
+export class Category {
+    constructor(
+        public id?:number,
+        public nome?: string,
+        public description?: string
+    ){}
+}
